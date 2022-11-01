216e31adc9086c2e8448c7f8ee4921e9b762bdd2
henry.5 <henry.5@kakaoenterprise.com>
2022-11-01 22:00:10 +0900

    Kick off

---
+++ src/test/java/com/example/springdeepdive/SpringDeepDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdeepdive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeepDiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
