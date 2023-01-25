1cff778c3c1c9fed082e3faeade8fe3be8dea876
henry.5 <henry.5@kakaoenterprise.com>
2023-01-25 20:56:52 +0900

    Init project

--- src/main/java/com/example/springdeepdive/SpringDeepDiveApplication.java
+++ src/main/java/com/example/springdeepdive/SpringDeepDiveApplication.java
@@ -1,13 +1,22 @@
 package com.example.springdeepdive;
 
+import org.apache.logging.log4j.util.Base64Util;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class SpringDeepDiveApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDeepDiveApplication.class, args);
+
+        String result =Base64Util.encode("2309784:2e2188f67667f47cf233ab92c514d15e");
+
+        System.out.println(result);
+
+
     }
 
 }
+
--- src/main/java/com/example/springdeepdive/TestController.java
+++ src/main/java/com/example/springdeepdive/TestController.java
@@ -1,19 +1,23 @@
 package com.example.springdeepdive;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class SpringDeepDiveApplicationTests {
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
 
     @Autowired
     private ApplicationContext applicationContext;
 
-    @Test
-    public void contextLoads() throws Exception {
+
+    @GetMapping("/test")
+    public void register() {
         if (applicationContext != null) {
             String[] beans = applicationContext.getBeanDefinitionNames();
 
